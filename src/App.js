f10697867f14e250ed307c42c4b717dfa589ab09
David Aydin <david.aydin6@gmail.com>
2022-11-18 16:52:20 -0500

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,43 +1,47 @@
-import './App.css';
-import Home from './components/Home/Home';
-import Header from './components/Header/Header';
-import React from 'react';
-import { Route } from 'react-router-dom';
-import Login from './components/Login/Login';
-import Loggedin from './components/Loggedin/LoggedIn';
-import Camera from './components/Camera/Camera';
-import Picture from './components/Picture/Picture';
+import "./App.css";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import React from "react";
+import { Route } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Loggedin from "./components/Loggedin/LoggedIn";
+import Camera from "./components/Camera/Camera";
+import Picture from "./components/Picture/Picture";
 
 import { auth } from "./services/firebase";
 import { useState, useEffect } from "react";
 
 function App() {
-
-  const [ user, setUser ] = useState(null);
+  const [user, setUser] = useState(null);
   useEffect(() => {
-    auth.onAuthStateChanged(user => setUser(user));
+    auth.onAuthStateChanged((user) => setUser(user));
   }, []);
 
   return (
-
     <div>
-      <Header/>
-      <Route exact path='/'>
-      <Home/>
+      <Header />
+
+      <Route exact path="/">
+        <Home />
       </Route>
-      <Route exact path='/login'>
-        <Login/>
+
+      <Route exact path="/login">
+        <Login />
       </Route>
-      <Route exact path='/loggedin'>
-        <Loggedin/>
+
+      <Route exact path="/loggedin">
+        <Loggedin />
       </Route>
-      <Route exact path='/camera'>
-        <Camera/>
+
+      <Route exact path="/camera">
+        <Camera />
       </Route>
-      <Route exact path='/picture'>
+
+      <Route exact path="/picture">
         <Picture></Picture>
       </Route>
-  )
+    </div>
+  );
 }
 
 export default App;
