51d994280345e5b7aa7d919c0f75a5237c872179
daydin14 <david.aydin6@gmail.com>
2022-11-18 14:49:07 -0500

    firebase user state in app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { auth } from "./services/firebase";
+import { useState, useEffect } from "react";
+
 function App() {
+
+  const [ user, setUser ] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged(user => setUser(user));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
