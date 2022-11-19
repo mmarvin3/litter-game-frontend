7e6e396a461944266481f32051e3bf77448efe18
David Aydin <david.aydin6@gmail.com>
2022-11-18 23:31:28 -0500

    update home and login page, started side menu

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div class="App">
       <Header user={user} />
 
       <Route exact path="/">
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,26 @@
+import "../Home/Home.css";
 import { Link } from "react-router-dom";
 
 function Home() {
-    return (
-        <div className="home">
-            <h1>Welcome to CleanUp Crew</h1>
-            <Link to='/login'>Login to Continue</Link>
-            <h3>Login with Google</h3>
-            <h3>Continue as Guest</h3>
-        </div>
-    )
+  return (
+    <div class="home">
+      <h1>Welcome to CleanUp Crew</h1>
+      <h4>Pick up litter to brighten your community.</h4>
+      <h4>See your impact for a brighter world.</h4>
+
+      <button>
+        <h4>
+          <Link to="/login">Login to Continue</Link>
+        </h4>
+      </button>
+
+      <br />
+      <br />
+      <button>
+        <h4>Explore as Guest</h4>
+      </button>
+    </div>
+  );
 }
 
 export default Home;
