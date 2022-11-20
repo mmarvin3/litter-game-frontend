7aae7b2c47d2d0502b1f7ae33de971f955a6936b
daydin14 <david.aydin6@gmail.com>
2022-11-20 12:24:27 -0500

    picture

--- src/pages/Picture/Picture.js
+++ src/pages/Picture/Picture.js
@@ -1,13 +1,17 @@
+import "./Picture.css";
 import { Link } from "react-router-dom";
 
 function Picture() {
     return (
-        <div>
+        <div class="picture">
+            <img src="https://i0.wp.com/bkreader.com/wp-content/uploads/2020/08/3738821611_3a2c334f2f_c.jpg?fit=800%2C536&ssl=1" alt="trash" id="pic" width="300px"/>
             <h2>Keep this picture retake?</h2>
-            <button>Retake</button>
-            <Link to ='/timer'>
-                <button>Keep</button>
-            </Link>
+            <div id="buttons">
+                <button>Retake</button>
+                <Link to ='/timer'>
+                    <button>Keep</button>
+                </Link>
+            </div>    
         </div>
     )
 }
