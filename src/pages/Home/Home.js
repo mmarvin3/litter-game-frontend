a35e13a03baf7b7f8b215e25d7e95980829e78c0
daydin14 <david.aydin6@gmail.com>
2022-11-20 11:39:19 -0500

    mulish google fonts, removed incorrect dependency for timer.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,19 +5,20 @@ function Home() {
   return (
     <div class="home">
       <h1>Welcome to CleanUp Crew</h1>
+
+      <br/>
+      
       <h4>Pick up litter to brighten your community.</h4>
       <h4>See your impact for a brighter world.</h4>
 
       <button>
-        <h4>
           <Link to="/login">Login to Continue</Link>
-        </h4>
       </button>
 
       <br />
       <br />
       <button>
-        <h4>Explore as Guest</h4>
+        Explore as Guest
       </button>
     </div>
   );
--- src/pages/Timer/Timer.js
+++ src/pages/Timer/Timer.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import reactRouterDom from "react-router-dom";
 import { Link } from "react-router-dom";
 
 function Timer() {
