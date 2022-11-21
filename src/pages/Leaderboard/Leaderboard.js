11b390d0002bde711b6a25c09fe3ac5e7a253cc7
mmarvin3 <mmerida336@gmail.com>
2022-11-20 23:34:40 -0500

    styling updates: gradient for leaderboard

--- src/pages/Leaderboard/Leaderboard.js
+++ src/pages/Leaderboard/Leaderboard.js
@@ -1,13 +1,16 @@
 // import { Link } from "react-router-dom";
 import "../Picture/Picture.css"
+import "../Leaderboard/Leaderboard.css"
 
 function Leaderboard() {
     return (
-       <div className="picture">
-           <h1>CleanUp Leaderboard</h1>
-           <img src="https://i.gyazo.com/a89f8135c6c06bd1e46b6233547754fb.png"></img>
-           <img src="https://i.gyazo.com/91f5b0c0908f6bb4c9c32d8336874366.png"></img>
-       </div> 
+        <div className="leaderboard-background">
+            <div className="picture">
+                <h1>CleanUp Leaderboard</h1>
+                <img src="https://i.gyazo.com/a89f8135c6c06bd1e46b6233547754fb.png"></img>
+                <img src="https://i.gyazo.com/fd69ae5d88a472f89639c16a5b5ae6bf.png"></img>
+            </div>
+        </div>
     )
 }
 
